8abe7fcaf742b0d076a8f3ef48bfd2daec009a8e
Yun Chan <YYunChan@Yuns-MacBook-Air.local>
2021-02-07 19:38:19 -0500

    Assingment 1 Part 1

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,19 +1,23 @@
+def listDivide (numbers, divide = 2):
+    d = 0
+    for x in numbers:
+        if x % divide == 0:
+            d += 1
+    return d
 
-def listDivide(numbers, divide):
-    """
-    The function returns the number of elements in the numbers list that are divisibleby divide
-    """
+class ListDivideException (Exception):
     pass
 
-def testListDivide():
-    """
-    Test listDivide
-    """
-    assert listDivide([1,2,3,4,5]) == 2
-    assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
-    assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
-    
-if __name__ == "__main__":
-    testListDivide()
+def testListDivide ():
+    if listDivide ([1, 2, 3, 4, 5]) != 2:
+        raise ListDivideException ()
+    elif listDivide ([2, 4, 6, 8, 10]) != 5:
+        raise ListDivideException ()
+    elif listDivide ([30, 54, 63, 98, 100], divide = 10) != 2:
+        raise ListDivideException ()
+    elif listDivide ([]) != 0:
+        raise ListDivideException ()
+    elif listDivide ([1, 2, 3, 4, 5], 1) != 5:
+        raise ListDivideException ()
+
+testListDivide()
