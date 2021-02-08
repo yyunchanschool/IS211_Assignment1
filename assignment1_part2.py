185af4059a8aeb05b673e3206a258df06f8d52d7
Yun Chan <YYunChan@Yuns-MacBook-Air.local>
2021-02-07 19:47:29 -0500

    Assignment 1 Part 2

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,10 +1,14 @@
+class Book():
+    author = ""
+    title = ""
+    def __init__ (self, author, title):
+        self.author = author
+        self.title = title
+    def display (self):
+        print (str(self.title + ", written by " + self.author))
 
-class Book:
-    def __init(self, author, title):
-        pass
-    
-    def display(self):
-        pass
-        
-if __name__ == "__main__":
-    pass
+object1 = Book ("John Steinbeck", "Of Mice and Men")
+object2 = Book ("Harper Lee", "To Kill a Mockingbird")
+
+object1.display()
+object2.display()
